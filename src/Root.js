cbaadbe19f23d3af7591cb370d2819bc33e258ca
s-nakai <s-nakai@nisso.co.jp>
2019-12-13 14:34:50 +0900

    add default header to axios

--- src/Root.js
+++ src/Root.js
@@ -8,6 +8,7 @@ import { Questions } from "./components/Questions";
 import { Start } from "./components/Start";
 import { Result } from "./components/Result";
 import axios from "axios";
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
 export const Root = () => {
   const [answers, setAnswers] = useState([]);
