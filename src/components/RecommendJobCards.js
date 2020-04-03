e2dcec8905d74ff88ce542f3a97925052d3e35fd
s-nakai <s-nakai@nisso.co.jp>
2020-04-03 16:09:54 +0900

    add slick

--- src/components/RecommendJobCards.js
+++ src/components/RecommendJobCards.js
@@ -7,16 +7,18 @@ import {
     CardContent,
     CardActions,
     Typography,
-    Grid,
     Container,
     Link,
     Button
 } from '@material-ui/core';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const useStyles = makeStyles(theme => ({
     root: {
         maxWidth: 400,
-        marginBottom: "1rem"
+        marginBottom: "2rem"
     },
     media: {
         height: 200,
@@ -31,7 +33,25 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export const RecommendJobCards = ({ recommendJobs }) => {
-    console.log(recommendJobs);
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay:true,
+        autoplaySpeed:5000,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow:1,
+                }
+            },
+        ]
+      };
     const classes = useStyles();
     const cards = recommendJobs.map(job => {
         const points = job.points.map(point => {
@@ -42,58 +62,50 @@ export const RecommendJobCards = ({ recommendJobs }) => {
             );
         });
         
-        const jumpLink = () => {
-            window.open(job.page_link, '_blank');
-        };
-
         return (
-            <Grid key={job.title} item xs={12} sm={4}>
-                <Card className={classes.root}>
-                    <CardActionArea
-                        onClick={() => jumpLink()}
-                    >
-                        <CardMedia
-                            className={classes.media}
-                            image={job.image_path}
-                            title={`image${recommendJobs.indexOf(job) + 1}`}
-                        />
-                        <CardContent className={classes.jobText}>
-                            <Typography
-                                gutterBottom
-                                variant="subtitle1"
-                                className={classes.jobTitle}
-                            >
-                                {job.title}
-                            </Typography>
-                            {points}
-                            <Typography variant="body2" gutterBottom>
-                                【作業内容】{job.content}
-                            </Typography>
-                            <Typography variant="body2">
-                                【勤務場所】{job.location}
-                            </Typography>
-                            <Typography variant="body2">
-                                【勤務時間】{job.shift}
-                            </Typography>
-                            <Typography variant="body2">
-                                【休　　日】{job.holiday}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Link
-                            variant="body2"
-                            target="_blank"
-                            rel="noopener"
-                            href={job.page_link}
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image={job.image_path}
+                        title={`image${recommendJobs.indexOf(job) + 1}`}
+                    />
+                    <CardContent className={classes.jobText}>
+                        <Typography
+                            gutterBottom
+                            variant="subtitle1"
+                            className={classes.jobTitle}
                         >
-                            <Button variant="outlined" color="secondary">
-                                詳細を見る
-                            </Button>
-                        </Link>
-                    </CardActions>
-                </Card>
-            </Grid>
+                            {job.title}
+                        </Typography>
+                        {points}
+                        <Typography variant="body2" gutterBottom>
+                            【作業内容】{job.content}
+                        </Typography>
+                        <Typography variant="body2">
+                            【勤務場所】{job.location}
+                        </Typography>
+                        <Typography variant="body2">
+                            【勤務時間】{job.shift}
+                        </Typography>
+                        <Typography variant="body2">
+                            【休　　日】{job.holiday}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Link
+                        variant="body2"
+                        target="_blank"
+                        rel="noopener"
+                        href={job.page_link}
+                    >
+                        <Button variant="outlined" color="secondary">
+                            詳細を見る
+                        </Button>
+                    </Link>
+                </CardActions>
+            </Card>
         )
     })
     return (
@@ -101,9 +113,9 @@ export const RecommendJobCards = ({ recommendJobs }) => {
             <Typography gutterBottom variant="h5" component="h2">
                 あなたにおすすめのお仕事！
             </Typography>
-            <Grid container justify="center">
-                {cards}
-            </Grid>
+                <Slider {...settings}>
+                    {cards}
+                </Slider>
         </ Container>
     )
 };
