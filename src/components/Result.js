1f91087d60de66d3daef804ac0489472c3eeceba
s-nakai <s-nakai@nisso.co.jp>
2020-04-15 15:34:05 +0900

    change image public #2

--- src/components/Result.js
+++ src/components/Result.js
@@ -22,7 +22,7 @@ export const Result = ({ answers, resetAnswers, calcResult, recommendJobs, reset
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:site', content: '@717450NISSO' },
         { property: 'og:type', content: 'article'},
-        { property: 'og:image', content: image1 },
+        { property: 'og:image', content: `${process.env.PUBLIC_URL}/image1.png` },
         { property: 'og:title', content: `あなたは${topScoreTitle}タイプです | 適職診断テスト 工場求人ナビ` },
         { property: 'og:description', content: 'あなたに合ったお仕事を探せる！' },
         { property: 'og:url', content: 'https://www.717450.net/' }
