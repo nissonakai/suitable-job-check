a5cf721487395641a31cc1e0bf9f403deab0e475
s-nakai <s-nakai@nisso.co.jp>
2020-04-15 15:49:50 +0900

    test #1

--- src/components/Result.js
+++ src/components/Result.js
@@ -21,7 +21,7 @@ export const Result = ({ answers, resetAnswers, calcResult, recommendJobs, reset
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:site', content: '@717450NISSO' },
         { property: 'og:type', content: 'article'},
-        { property: 'og:image', content: `${process.env.PUBLIC_URL}/image1.png` },
+        { property: 'og:image', content: 'https://suitable-job-checker-nisso.netlify.com/image1.png' },
         { property: 'og:title', content: `あなたは${topScoreTitle}タイプです | 適職診断テスト 工場求人ナビ` },
         { property: 'og:description', content: 'あなたに合ったお仕事を探せる！' },
         { property: 'og:url', content: 'https://www.717450.net/' }
@@ -40,6 +40,8 @@ export const Result = ({ answers, resetAnswers, calcResult, recommendJobs, reset
         resetAndBacktoHome();
         return false;
     };
+
+    console.log(`パブリック ${process.env.PUBLIC_URL}`);
 
     return (
         <>
