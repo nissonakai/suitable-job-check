f3ec22f00c5ee12b05e153eec133031740aaff56
s-nakai <s-nakai@nisso.co.jp>
2020-04-16 13:38:50 +0900

    change url for netlify

--- src/components/Result.js
+++ src/components/Result.js
@@ -21,11 +21,11 @@ export const Result = ({ answers, resetAnswers, calcResult, recommendJobs, reset
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:site', content: '@717450NISSO' },
         { property: 'og:type', content: 'article'},
-        { property: 'og:image', content: 'https://suitable-job-checker-nisso.netlify.com/image1.png' },
+        { property: 'og:image', content: `${process.env.REACT_APP_PUBLIC_URL}/image1.png` },
         { property: 'og:title', content: `あなたは${topScoreTitle}タイプです | 適職診断テスト 工場求人ナビ` },
         { property: 'og:description', content: 'あなたに合ったお仕事を探せる！' },
         { property: 'og:url', content: 'https://www.717450.net/' }
-      ]
+      ];
 
     const history = useHistory();
     const classes = useStyles();
@@ -40,8 +40,6 @@ export const Result = ({ answers, resetAnswers, calcResult, recommendJobs, reset
         resetAndBacktoHome();
         return false;
     };
-
-    console.log(`パブリック ${process.env.PUBLIC_URL}`);
 
     return (
         <>
